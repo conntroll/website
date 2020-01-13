527aeac509158153d49531d19c19b486dd423c8b
navigaid <navigaid@gmail.com>
2020-01-14 02:38:00 +0800

    update genk0s.sh

--- genk0s.sh
+++ genk0s.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+trap 'cleanup' EXIT INT TERM
+
 sed_rules(){
   VERSION=$1
   cat <<EOF # | tee /dev/stderr
@@ -15,14 +17,19 @@ s,https://v1-17.docs.kubernetes.io,/1.17,g
 EOF
 }
 
-main(){
+prepare(){
+  test -f hugo || wget -qO- https://github.com/gohugoio/hugo/releases/download/v0.62.2/hugo_extended_0.62.2_Linux-64bit.tar.gz | tar xz hugo
+  export PATH=$PWD:$PATH
+}
+
+loop(){
   # for version in 1.1{4..7}; do
   # for version in 1.1{3,6}; do
   for version in 1.1{3..7}; do
   # for version in 1.17; do
-    echo "============== Making $version"
+    echo "============================================== Making $version"
     rm -rf $version
-    git branch -D "k0s-${version}"
+    git branch -D "k0s-${version}" &>/dev/null || :
     git checkout release-$version
     git checkout -b k0s-$version release-$version
     sed -i -f <(sed_rules $version) config.toml layouts/partials/head.html
@@ -30,6 +37,19 @@ main(){
     mv public $version
     git checkout -f .
   done
+}
+
+pack(){
+  echo tar c k0s.tar 1.1{3..7}
+}
+
+main(){
+  prepare
+  loop
+  pack
+}
+
+cleanup(){
   git checkout k0s
 }
 
